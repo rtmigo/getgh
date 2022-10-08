feb58a8215478317571dcb13144e82618b6a13fa
Artem IG <ortemeo@gmail.com>
2022-10-08 09:42:28 +0300

    upd

--- test/funcs_test.dart
+++ test/funcs_test.dart
@@ -13,14 +13,14 @@ void main() {
 
   test('gh api either left', () {
     final r = ghApi(argToEndpoint(
-        "https://github.com/rtmigo/hflakdhf/blob/91273/README.md"));
+        "https://github.com/rtmigo/hflakdhf/blob/91273/README.md").right);
     expect(r.isLeft, true);
     expect(() => r.right, throwsException);
   });
 
   test('gh api success', () {
     final e = ghApi(argToEndpoint(
-        "https://github.com/rtmigo/jsontree_dart/blob/staging/README.md"));
+        "https://github.com/rtmigo/jsontree_dart/blob/staging/README.md").right);
     expect(e.isRight, true);
     expect(bytesToGhSha(e.right.content()), e.right.sha);
   });
