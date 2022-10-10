bcd1667bc6539cce6735e31ddb20d3a5873b4ce6
Artem IG <ortemeo@gmail.com>
2022-10-10 03:06:24 +0300

    - removed Either (replaced with exceptions) - can download whole directories now

--- test/urls_test.dart
+++ test/urls_test.dart
@@ -1,23 +1,43 @@
+import 'package:kt_dart/collection.dart';
 import 'package:test/test.dart';
 
 import '../bin/source/gh_api.dart';
+import '../bin/source/github_urls.dart';
 
 void main() {
-  test('remove blob and branch', () {
-    expect(
-        removeBlobAndBranch("user/repo/blob/branch/file.ext".split("/")),
-        ["user", "repo", "file.ext"]);
+  group('URL segments to user/repo/dir/file', () {
+    test('from blob', () {
+      expect(
+          GithubPathSegments("user/repo/blob/branch/dir/file.ext".split("/").kt)
+              .withoutBlobAndBranch(),
+          ["user", "repo", "dir", "file.ext"].kt);
+    });
+
+    test('from tree', () {
+      expect(
+          GithubPathSegments("user/repo/tree/branch/dir".split("/").kt)
+              .withoutBlobAndBranch(),
+          ["user", "repo", "dir"].kt);
+    });
+
+    test('from tree ending with slash', () {
+      expect(
+          GithubPathSegments("user/repo/tree/branch/dir/".split("/").kt)
+              .withoutBlobAndBranch(),
+          ["user", "repo", "dir"].kt);
+    });
   });
 
   test('argToEndpoint', () {
-    expect(argToEndpoint("https://github.com/rtmigo/cicd/blob/dev/stub.py").right.string,
+    expect(
+        argToEndpoint("https://github.com/rtmigo/cicd/blob/dev/stub.py")
+            .string,
         "/repos/rtmigo/cicd/contents/stub.py?ref=dev");
 
-
-    expect(argToEndpoint("https://github.com/rtmigo/cicd/stub.py").right.string,
+    expect(argToEndpoint("https://github.com/rtmigo/cicd/stub.py").string,
         "/repos/rtmigo/cicd/contents/stub.py");
 
-    expect(argToEndpoint("/repos/rtmigo/cicd/contents/stub.py").right.string,
+    expect(argToEndpoint("/repos/rtmigo/cicd/contents/stub.py").string,
         "/repos/rtmigo/cicd/contents/stub.py");
   });
 }
