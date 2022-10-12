d14269f8add6781de24af5391f1116e9c7b3af93
Artem IG <ortemeo@gmail.com>
2022-10-12 07:31:55 +0300

    removing 'gh:' prefix from errors

--- bin/source/exceptions.dart
+++ bin/source/exceptions.dart
@@ -3,6 +3,14 @@ class ExpectedException extends Error {
   final String message;
 }
 
+class GhErrorMessage extends ExpectedException {
+  GhErrorMessage(final String stderr): super(_trim(stderr));
+
+  static String _trim(final String stderr) {
+    return stderr.trim().replaceAll(RegExp("gh:\\s+"), "");
+  }
+}
+
 class UnsupportedContentTypeException extends ExpectedException {
-  UnsupportedContentTypeException(String s): super(s);
+  UnsupportedContentTypeException(final String s): super(s);
 }
--- test/errors_test.dart
+++ test/errors_test.dart
@@ -1,12 +1,14 @@
 import 'package:kt_dart/collection.dart';
 import 'package:test/test.dart';
 
+import '../bin/source/exceptions.dart';
 import '../bin/source/gh_api.dart';
 
 void main() {
   test("call with gh", () async {
     (await listRemoteEntries(argToEndpoint(
-            "https://github.com/rtmigo/ghfile_test_data/blob/dev/"))).dart;
+            "https://github.com/rtmigo/ghfile_test_data/blob/dev/")))
+        .dart;
   });
 
   test("call without gh", () {
@@ -17,5 +19,16 @@ void main() {
                 executable: "gh_non_existent"))
             .dart,
         throwsA(isA<GhNotInstalledException>()));
+  });
+
+  group("gh error trimming", () {
+    test("single line", () {
+      expect(GhErrorMessage("gh: Not Found (HTTP 404)").message,
+          "Not Found (HTTP 404)");
+    });
+    test("multi line", () {
+      expect(GhErrorMessage("gh: Not Found (HTTP 404)\ngh:  Oops!").message,
+          "Not Found (HTTP 404)\nOops!");
+    });
   });
 }
